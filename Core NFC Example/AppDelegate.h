87429dfd31013ff415d2e270470d8e5de94ed3df
Cai <zw.cai@mediatek.com>
2017-06-13 14:34:41 +0800

    Initial Commit

---
+++ Core NFC Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core NFC Example
+//
+//  Created by Cai on 13/06/2017.
+//  Copyright © 2017 Zhi-Wei Cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Core NFC Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core NFC Example
+//
+//  Created by Cai on 13/06/2017.
+//  Copyright © 2017 Zhi-Wei Cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
